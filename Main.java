f0bfdc04c5d055939066f1aba64d301e7f585d21
Sizz8K <sizz8k@hotmail.com>
2019-12-07 11:40:22 +0000

    Change

--- Main.java
+++ Main.java
@@ -6,6 +6,10 @@ public class Main {
     public static void main(String[] args) {
         for (String s : args){
             System.out.println(s);
+            System.out.println("pierwsza zmiana");
+            System.out.println("");
+            System.out.println(".");
+
         }
 	// write your code here
     }
